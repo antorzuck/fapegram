4de2f80abbd6b9f5d3879580ac539e3f60765a8c
Antor <antorzuck@gmail.com>
2023-12-24 07:54:44 -0500

    -_-

--- base/models.py
+++ base/models.py
@@ -1,16 +1,45 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-class Art(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
 
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    dp = models.FileField(upload_to='dp')
-    name = models.CharField(max_length=100)
-    bio = models.TextField()
+    dp = models.FileField(upload_to='dp', null=True, blank=True)
+    name = models.CharField(max_length=100, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
     onlyfans = models.URLField(null=True, blank=True)
     twitter = models.URLField(null=True, blank=True)
     insta = models.URLField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
+
+
+class Art(models.Model):
+    prof = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    image = models.FileField(upload_to='contents', null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user.username)
+    
+
+class Vids(models.Model):
+    prof = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    video = models.FileField(upload_to='videos', null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user.username)
+    
+
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance, name=instance.username)
 
--- base/views.py
+++ base/views.py
@@ -3,7 +3,9 @@ from base.models import *
 
 
 def home(request):
-    return render(request, 'home.html')
+    img = Art.objects.all().order_by('-id')
+    context = {'img': img}
+    return render(request, 'home.html', context)
 
 def profile(request):
     return render(request, 'profile.html')
